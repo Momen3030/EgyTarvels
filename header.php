85dae7a3a77811801f5d837727274b7c58e2af4e
Momen3030 <momenadel3030@gmail.com>
2019-03-09 10:36:49 +0200

    v1.9

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>  
   <link rel="stylesheet" href="css/main.css">
+  <link rel="shortcut icon" href="images/favicon.ico">
 
   
   <link rel='stylesheet' type='text/css' href='css/jquery.kwicks.css' />
